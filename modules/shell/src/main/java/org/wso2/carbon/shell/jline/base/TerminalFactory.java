ca7be03b623d4005d842391e25cc320075b20e9c
Pubudu Dissanayake <pubudu08@gmail.com>
2014-05-22 18:12:09 +0530

    Modified TerminalFactory class to add UNIX like terminal wrapper from jline

--- modules/shell/src/main/java/org/wso2/carbon/shell/jline/base/TerminalFactory.java
+++ modules/shell/src/main/java/org/wso2/carbon/shell/jline/base/TerminalFactory.java
@@ -1,7 +1,34 @@
 package org.wso2.carbon.shell.jline.base;
 
+import jline.NoInterruptUnixTerminal;
+import jline.Terminal;
+
 /**
- * Created by pubudu on 5/22/14.
+ * TerminalFactory represents initial jline terminal
  */
 public class TerminalFactory {
+
+    private Terminal terminal;
+
+    public synchronized Terminal getTerminal() throws Exception{
+        if(terminal==null){
+            init();
+        }
+        return terminal;
+    }
+
+    public void init(){
+        jline.TerminalFactory.registerFlavor(jline.TerminalFactory.Flavor.UNIX, NoInterruptUnixTerminal.class);
+        terminal = jline.TerminalFactory.create();
+
+    }
+
+    public synchronized void destroy()throws Exception{
+        if(terminal!=null){
+            terminal.restore();
+            terminal=null;
+
+        }
+
+    }
 }
